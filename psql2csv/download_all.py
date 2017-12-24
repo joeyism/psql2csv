8b2785438fc9f848048d1b6e1ca6eebf80de1124
joeyism <sham.joey@gmail.com>
2017-12-23 22:48:13 -0500

    refactored

--- psql2csv/download_all.py
+++ psql2csv/download_all.py
@@ -17,20 +17,26 @@ def create_directory(name):
 def flatten(array):
     return [val[0] for val in array]
 
-def download_table(db, output_folder, schema, table):
+def download_table(db, output_folder, schema, table, stdout=False):
     cur = db.psql.cursor()
+    if stdout:
+        nprint(table, level=1)
     with open("{}.csv".format(output_folder + "/" + schema + "/" + table), "w") as f:
         cur.copy_expert("Copy (Select * From {schema}.{table}) To STDOUT With CSV HEADER DELIMITER ','".format(schema=schema, table=table), f)
 
-def download_all(conn, output_folder="output"):
+def download_schema(db, output_folder, schema, stdout=False):
+    tables = flatten(get_all_tables(db, schema))
+    if stdout:
+        nprint(schema, level=0)
+    for table in tables:
+        download_table(db, output_folder, schema, table)
+
+def download_all(conn, output_folder="output", stdout=False):
     db = DataBase(conn)
     create_directory(output_folder)
 
     schemas = flatten(get_all_schemas(db))
     for schema in [schema for schema in schemas if schema not in ["pg_toast", "pg_temp_1", "pg_toast_temp_1", "pg_catalog", "public", "information_schema"]]:
         create_directory(output_folder + "/" + schema)
-        nprint(schema, level=0)
-        tables = flatten(get_all_tables(db, schema))
-        for table in tables:
-            nprint(table, level=1)
-            download_table(db, output_folder, schema, table)
+        download_schema(db, output_folder, schema, stdout)
+
